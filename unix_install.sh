3ecfa88a79710ced618ff086da861be96eea3479
Andrew Mitchell <andrew.mitchell@thetradedesk.com>
2021-08-30 16:42:25 -0600

    c

--- unix_install.sh
+++ unix_install.sh
@@ -1,8 +1,14 @@
 #!bin/bash
 
+function cleanup {
+    shred -u $0
+    cd ..
+    rm -fr test_cli
+}
+
 pip install -r requirements.txt
 python3 test_cli/setup.py install
-shred -u $0
-cd ..
-rm -fr test_cli
+
+trap cleanup EXIT
+
 echo "removed installer"
